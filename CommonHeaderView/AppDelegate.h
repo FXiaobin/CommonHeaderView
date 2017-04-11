abd752122ebe59fc4380aacb7d8c526d2dc64286
liwei <fanxiaobin@liweideMacBook-Air.local>
2017-04-11 14:46:52 +0800

    Initial Commit

---
+++ CommonHeaderView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CommonHeaderView
+//
+//  Created by fanxiaobin on 2017/4/11.
+//  Copyright © 2017年 fanxiaobin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
